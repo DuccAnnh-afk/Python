86a78c1c54a3ad7e3eb37ac8f2ef7ec35c63e592
Đào Đức Anh <xihaotungcua@gmail.com>
2026-03-26 13:19:12 +0700

    Add files via upload

---
+++ 2_3_Bai1.py
@@ -0,0 +1,7 @@
+# Bài 1: Tính tổng 2 số nguyên
+
+a = int(input("Nhập số a: "))
+b = int(input("Nhập số b: "))
+
+tong = a + b
+print("Tổng =", tong)
---
+++ 2_3_Bai5.py
@@ -0,0 +1,10 @@
+# Bài 5: Tính chu vi và diện tích hình tròn
+
+pi = 3.14
+R = float(input("Nhập bán kính: "))
+
+chu_vi = 2 * pi * R
+dien_tich = pi * R * R
+
+print("Chu vi =", chu_vi)
+print("Diện tích =", dien_tich)
