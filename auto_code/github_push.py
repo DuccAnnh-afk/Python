ea0377d88a017652d620356ed169f975eb9ca134
Dao Duc Anh <xihaotungcua@gmail.com>
2026-03-31 08:00:09 +0700

    auto add bai tap

--- auto_code/github_push.py
+++ auto_code/github_push.py
@@ -1,19 +1,33 @@
 import os
 
+def run(cmd):
+    print(f"> {cmd}")
+    return os.system(cmd)
+
 def push_github():
     try:
-        # lấy đường dẫn file hiện tại
         current_dir = os.path.dirname(os.path.abspath(__file__))
-
-        # đi về root (luuthetoan)
         root_dir = os.path.abspath(os.path.join(current_dir, ".."))
-
         os.chdir(root_dir)
 
-        os.system("git add .")
-        os.system('git commit -m "auto add bai tap"')
-        os.system("git push")
+        print(" Đang ở:", root_dir)
 
-        print("Đã push lên GitHub!")
+      
+        run("git stash")
+
+       
+        run("git pull origin main")
+
+        run("git stash pop")
+
+     
+        run("git add .")
+
+        commit_status = run('git commit -m "auto add bai tap"')
+        
+        run("git push origin main")
+
+        print("✅ Đã push lên GitHub!")
+
     except Exception as e:
-        print("Lỗi:", e)
+        print("❌ Lỗi:", e)
