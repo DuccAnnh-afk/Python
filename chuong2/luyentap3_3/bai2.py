5252b44aba7d25531b85a8fd953dd71f812dc086
DuccAnnh-afk <xihaotungcua@gmail.com>
2026-03-31 07:33:53 +0700

    auto add bai tap

--- chuong2/luyentap3_3/bai2.py
+++ chuong2/luyentap3_3/bai2.py
@@ -1,6 +1,6 @@
-a = int "nhập cạnh a:
-b = int "nhập cạnh b:
-c = int "nhập cạnh c:
+a = int(input("Nhập a: "))
+b = int(input("Nhập b: "))
+c = int(input("Nhập c: "))
 if a + b > c and a + c > b and b + c > a:
     print("Ba cạnh này có thể tạo thành một tam giác.")
 else:
